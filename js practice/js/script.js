3eff1c8f40559ad79098098b6ed58100fd9537f0
Begimai <begimai.akulova@gmail.com>
2020-11-25 23:45:32 +0300

    first message

---
+++ js practice/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+ 
+// alert("hello");
+
+// const result=confirm("Are you here?");
+// console.log(result);
+
+// const answer=prompt("Vam est 18?", "enter your answer");
+// console.log(typeof(answer));
+
+// const answer=+prompt("Vam est 18?"); use "+" to make integer!!!
+// console.log(answer+5);
+
+const category="toys";
+console.log(`https.someurl.com${category}/6/7`);
+
+const user="Ivan";
+alert(`Hello ${user}`);
