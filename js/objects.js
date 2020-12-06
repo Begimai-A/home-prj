5704b7a0a8c415465aafe9fb62665c1a6c70349b
Begimai <begimai.akulova@gmail.com>
2020-12-07 02:08:54 +0300

    add new exercises with objects, like friends and karyz

---
+++ js/objects.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// exercises with objects
+const friends=[
+  {name:"Anna", age:21, luckyNumbers:[1,5,11,4,9]},
+  {name:"Dave", age: 33, luckyNumbers:[9,5, 4, 7, 6] },
+  {name:"Kate", age: 27, luckyNumbers:[4,7, 1, 3, 5] }
+];
+
+ console.log(friends[1].luckyNumbers);
+
+ // DEBT program exercise
+
+ const karyz={
+    Jimmi:5,
+    Anna:3,
+    Jacob:10
+ };
+ karyz.Jimmi+=3; // used operator (+=) 
+ console.log(karyz);
+
+
