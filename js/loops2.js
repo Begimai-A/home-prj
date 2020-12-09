1bc6aa15d77bc30d423694245e692a69e82c6ffa
Begimai <begimai.akulova@gmail.com>
2020-12-10 00:19:52 +0300

    add awesome animal,random string and hacker speak programs

---
+++ js/loops2.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let animals=["cat", "fish","dog", "rabbit"];
+
+for ( let i=0; i<animals.length; i++) {
+    console.log( animals[i] + " is an awesome animal" + "," );
+
+}
+
+
+var abc = "abcdefghijklmnopqrstuvwxyz";
+        var rs = "";
+        while (rs.length < 6) {
+            rs += abc[Math.floor(Math.random() * abc.length)];
+        }
+        console.log(rs);
+
+// h4ck3r sp34k / HACKER SPEAK
+
+let input="javascript is awesome";
+let output="";
+
+for( let i=0; i<input.length; i++) {
+    if (input[i]==="a") {
+        output+=4;
+    } else if (input[i]==="e"){
+        output+=3;
+    }else if (input[i]==="o"){
+        output+=0;
+    }else if  (input[i]==="i"){
+        output+=1;
+    } else {
+        output+=input[i];
+    }
+}
+console.log(output);
+
+
+
