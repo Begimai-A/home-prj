5847fb01aa7151ec99112fb8f6a10385114ce9d4
Begimai <begimai.akulova@gmail.com>
2020-12-19 01:19:08 +0300

    add arrays with the help of the functions and practised callback function

--- js/functions2.js
+++ js/functions2.js
@@ -16,4 +16,67 @@ let result=add(multiply(36325, 9824), 777);
 
 console.log(result);
 
+// 2nd exercise "Совпадают ли массивы?"
+
+let areArraysSame=function (array1, array2) {
+//   if (array1.length!=array2.length) {
+//       return false;
+//   }
+
+  for (let i=0; i<array1.length; i++){
+      if (array1[i] !=array2[i] || array1.length!=array2.length ) {
+          return false;
+      }
+  }
+  return true;
+};
+
+let arraysCheck=areArraysSame ([1, 2, 3], [1, 2, 3 ]);
+
+console.log(arraysCheck);
+
+let areWordsSame=function (word1, word2) {
+    //   if (array1.length!=array2.length) {
+    //       return false;
+    //   }
+    
+      for (let i=0; i<word1.length; i++){
+          if (word1[i] !=word2[i] || word1.length!=word2.length ) {
+              return false;
+          }
+      }
+      return true;
+    };
+    
+    let wordCheck=areWordsSame ("Anna", "anna");
+    
+    console.log(wordCheck);
+
+// 3rd exercise "Виселица и функции"
+
+// Callback function
+
+function doHomework( subject, callback) {
+    console.log(`I am studying ${subject} homework.`);
+    callback();
+}
+
+doHomework("math", function(){
+    console.log ("I finished studying");
+});
+
+// callback function example 2
+
+function activity (subj, callback) {
+    console.log(`I studied ${subj} very well`);
+    callback();
+}
+
+activity("chemistry", function(){
+    console.log("That's why I passed the exam");
+});
+
+
+
+
 
